4c247db5355f377ceac4bbb56f4c96396a922d64
robertsteele7 <atpcycle7@gmail.com>
2022-11-23 13:45:19 -0600

    finished view product and update product

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -1,11 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+using Testing.Models;
+
+
+
 
 namespace Testing
 {
@@ -14,6 +20,52 @@ namespace Testing
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddScoped<IDbConnection>((s) =>
+            {
+                IDbConnection conn = new MySqlConnection(builder.Configuration.GetConnectionString("bestbuy"));
+                conn.Open();
+                return conn;
+
+            });
+
+            builder.Services.AddTransient<IProductRepository, ProductRepository>();
+
+            var app = builder.Build();
+
+
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+
+
+
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+
+          
+            app.Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
